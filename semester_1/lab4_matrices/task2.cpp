d8ecc1472fa3833dc949f026d98ebe5bc47adaab
Rodrygo <romanyukivan404@gmail.com>
2025-11-10 20:18:20 +0300

    cpp code is changed

--- semester_1/lab4_matrices/task2.cpp
+++ semester_1/lab4_matrices/task2.cpp
@@ -3,7 +3,43 @@
 
 const int MAX_SIZE_matrix = 500;
 
-void PrintOfMatrix (int matrix[MAX_SIZE_matrix][MAX_SIZE_matrix], int n) {
+void readMatrix (int** matrix, int n) {
+
+    std::cout << "enter n*n elements of matrix:\n";
+
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[n];
+        for (int j = 0; j < n; j++)
+            if (!(std::cin >> matrix[i][j]))
+                throw "ERROR!!! You must enter number";
+    }
+
+}
+
+void fillMatrixRandom (int** matrix, int n, std::mt19937& gener) {
+
+    std::cout << "enter right and left border of your random numbers: ";
+    int right_border, left_border;
+    if (!(std::cin >> left_border) || !(std::cin >> right_border))
+        throw "ERROR!!! You must enter number";
+
+
+    if (left_border > right_border)
+        std::swap(left_border, right_border);
+
+    std::uniform_int_distribution<int> dist(left_border, right_border);
+
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[n];
+        for (int j = 0; j < n; j++)
+            matrix[i][j] = dist(gener);
+    }
+
+    std::cout << "\nelements of an matrixay filled with random numbers from " <<  left_border << " to " << right_border << " :\n";
+
+}
+
+void PrintOfMatrix (int** matrix, int n) {
 
     std::cout << "elements of array:\n";
     for (int i = 0; i < n; i++) {
@@ -17,7 +53,7 @@ void PrintOfMatrix (int matrix[MAX_SIZE_matrix][MAX_SIZE_matrix], int n) {
 
 }
 
-void ReplaceZeroLinesOfArray(int matrix[MAX_SIZE_matrix][MAX_SIZE_matrix], int n) {
+void ReplaceZeroLinesOfArray(int** matrix, int n) {
 
     for (int i = 0; i < n; i++) {
         if (matrix[i][0] == 0) {
@@ -29,7 +65,7 @@ void ReplaceZeroLinesOfArray(int matrix[MAX_SIZE_matrix][MAX_SIZE_matrix], int n
 
 }
 
-int SerchMaxElementOfTopRightTriangle (int matrix[MAX_SIZE_matrix][MAX_SIZE_matrix], int n) {
+int SerchMaxElementOfTopRightTriangle (int** matrix, int n) noexcept {
 
     int mx = matrix[0][1];
     for (int i = 0; i < n; i++)
@@ -41,69 +77,59 @@ int SerchMaxElementOfTopRightTriangle (int matrix[MAX_SIZE_matrix][MAX_SIZE_matr
 
 int main()
 {
-    using std::cin;
-    using std::cout;
 
-    cout << "enter 0 if you want that matrixay consists of random numbers\nelse enter 1 to get matrixay consists of entered numbers\n";
-    bool flag;
-    if (!(cin >> flag)) {
-        cout << "ERROR!!!";
-        std::exit(1);
-    }
+    int n = 0;
+    int** matrix = nullptr;
 
-    cout << "enter the size of matrix[n*n] (n from 1 to 500): ";
-    int n;
-    if (!(cin >> n) || n < 1 || n > MAX_SIZE_matrix) {
-        cout << "ERROR!!!";
-        std::exit(1);
-    }
+    try {
 
-    int matrix[MAX_SIZE_matrix][MAX_SIZE_matrix];
+        std::cout << "enter 0 if you want that matrixay consists of random numbers\nelse enter 1 to get matrixay consists of entered numbers\n";
+        bool flag;
+        if (!(std::cin >> flag))
+            throw "ERROR!!! you must enter either 1 or 0";
 
-    if (flag) {
 
-        cout << "enter n*n elements of matrix:\n";
+        std::cout << "enter the size of matrix[n*n] (n from 1 to 500): ";
+        if (!(std::cin >> n)) {
+            throw "ERROR!!! You must enter number";
+        }
+        if (n < 1) {
+            throw "ERROR!!! There can be no less than 1 single element in an array";
+        }
+        if (n > MAX_SIZE_matrix) {
+            throw "ERROR!!! Matrix cant be [>500][>500]";
+        }
+        matrix = new int*[n];
 
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                if (!(cin >> matrix[i][j])) {
-                    cout << "ERROR!!!";
-                    std::exit(1);
-                }
-    }
-    else {
 
-        cout << "enter right and left border of your random numbers: ";
-        int right_border, left_border;
-        if (!(cin >> left_border) || !(cin >> right_border)) {
-            cout << "ERROR!!!";
-            std::exit(1);
+        if (flag)
+            readMatrix(matrix, n);
+        else {
+            std::mt19937 gen(23523);
+            fillMatrixRandom(matrix, n, gen);
         }
 
-        if (left_border > right_border)
-            std::swap(left_border, right_border);
 
-        std::mt19937 gen(2345);
-        std::uniform_int_distribution<int> dist(left_border, right_border);
 
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                matrix[i][j] = dist(gen);
 
-        cout << "elements of an matrixay filled with random numbers from " <<  left_border << " to " << right_border << " :\n";
+        PrintOfMatrix(matrix, n);
 
+        ReplaceZeroLinesOfArray(matrix, n);
+
+        std::cout << "Replaced!\n\n";
+        PrintOfMatrix(matrix, n);
+
+        int max_element_of_TopRight_triangle = SerchMaxElementOfTopRightTriangle(matrix, n);
+
+        std::cout << "max element of TopRight triangle is " << max_element_of_TopRight_triangle << '\n';
+    }
+    catch (const char* errorMes) {
+        std::cout << errorMes << '\n';
     }
 
-    PrintOfMatrix(matrix, n);
-
-    ReplaceZeroLinesOfArray(matrix, n);
-
-    cout << "Replaced!\n\n";
-    PrintOfMatrix(matrix, n);
-
-    int max_element_of_TopRight_triangle = SerchMaxElementOfTopRightTriangle(matrix, n);
-
-    cout << "max element of TopRight triangle is " << max_element_of_TopRight_triangle << '\n';
+    for (int i = 0; i < n; ++i)
+        delete[] matrix[i];
+    delete[] matrix;
 
     return 0;
 }
