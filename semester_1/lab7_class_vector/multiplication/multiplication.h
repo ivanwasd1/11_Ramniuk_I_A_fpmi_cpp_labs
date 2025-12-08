3f30a1829c3b02b7dfc6782e0d125cf831160bb7
Rodrygo <romanyukivan404@gmail.com>
2025-12-08 23:27:53 +0300

    modified:   lab7_class_vector/multiplication/multiplication.h

--- semester_1/lab7_class_vector/multiplication/multiplication.h
+++ semester_1/lab7_class_vector/multiplication/multiplication.h
@@ -1,7 +1,7 @@
 #pragma once
-
 #include <exception>
 
 int64_t Multiply(int32_t a, int32_t b) {
-    return std::static_cost<int64_t>(a)*std::static_cost<int64_t>(b);
+    return static_cast<int64_t>(a)*(b);
 }
+
