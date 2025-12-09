f56100e13a758c8e1a1df08e0a5d1529e3661005
Rodrygo <romanyukivan404@gmail.com>
2025-12-09 03:56:20 +0300

    fasd

--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -1,5 +1,41 @@
 #pragma once
+#include <iostream>
+#include <exception>
+#include <algorithm>
+#include <cstddef>
+#include <initializer_list>
 
 class Vector {
-    // your code goes here
+
+    private:
+        int* arr;
+        int arrSize;
+        int capacity;
+        void isOutRange(size_t idx) const;
+        void Reallocate(size_t newCapacity);
+    public:
+        Vector() noexcept;
+        explicit Vector(size_t n);
+        Vector(std::initializer_list<int> listik);
+        Vector(const Vector& other);
+        Vector& operator=(Vector other);
+        ~Vector();
+
+        void Swap(Vector& other) noexcept;
+        
+        const int& operator[](size_t idx) const noexcept; 
+        int& operator[](size_t idx) noexcept;
+
+        const int& At(size_t idx) const;
+        int& At(size_t idx);
+
+        size_t Size() const noexcept;
+        size_t Capacity() const noexcept;
+
+        void PushBack(int newNum);
+        void PopBack() noexcept;
+        void Clear() noexcept;
+        void Reserve(size_t newCapacity);
 };
+
+std::ostream& operator<<(std::ostream& out, const Vector& vector);
